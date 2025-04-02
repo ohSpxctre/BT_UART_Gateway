879c6fb180981889ecb2e0db910b580bf7eb44cf
Simon <119622323+ohSpxctre@users.noreply.github.com>
2025-04-02 16:54:47 +0200

    Set up test for system

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -10,6 +10,18 @@
 
 #include "uart.hpp"
 #include "MessageHandler.hpp"
+#include "DataParser.hpp"
+#include "CommandHandler.hpp"
+
+// Create a MessageHandler instance
+MessageHandler msgHandler;
+
+// Create a CommandHandler and DataParser instance
+CommandHandler cmdHandler;
+DataParser dataParser(cmdHandler);
+
+// Create a UART instance
+uart::Uart uart0;
 
 esp_pthread_cfg_t create_config(const char *name, int stack, int prio)
 {
@@ -21,44 +33,78 @@ esp_pthread_cfg_t create_config(const char *name, int stack, int prio)
     return cfg;
 }
 
-void uartTest_task()
+void uartReceiveTest_task()
 {
-    // Create a UART and MessageQueue object
-    uart::Uart uart;
-    //QueueHandle_t uart_queue;
+    // Buffer to store the received data
     char data[uart::UART_BUFFER_SIZE];
 
-    MessageHandler msgHandler;
-
-    MessageHandler::Message message{};
-    std::string msg = "Hello from UART!";
-    std::copy(msg.begin(), msg.end(), message.begin());
-
-    msgHandler.send(MessageHandler::QueueType::UART_QUEUE, message);
-    msgHandler.receive(MessageHandler::QueueType::UART_QUEUE, message);
-    ESP_LOGI(pcTaskGetName(nullptr), "Received message: %s", message.data());
-
-    uart.init(msgHandler.getUartEventQueue());
-    
     while (true)
     {
-        int bytesReceived = uart.receive(data);
+        // Buffer the received data till a carriage return or newline is received
+        int bytesReceived = 0;
+        bool isEndOfLine = false;
+        // Read data from the UART interface
+        while (!isEndOfLine && bytesReceived < uart::UART_BUFFER_SIZE - 1) {
+            bytesReceived += uart0.receive(data + bytesReceived);
+            // Re-send the received data back to the sender
+            uart0.send(data + bytesReceived - 1, 1);
+            // Check if the last character is a carriage return or newline
+            if (data[bytesReceived - 1] == '\r' || data[bytesReceived - 1] == '\n') {
+                isEndOfLine = true;
+            }
+        }           
+
+        MessageHandler::Message message;
+        std::copy(data, data + bytesReceived, message.begin());
+        msgHandler.send(MessageHandler::QueueType::DATA_PARSER_QUEUE, message, MessageHandler::ParserMessageID::MSG_ID_UART);
 
         // Log the received data
         ESP_LOGI(pcTaskGetName(nullptr), "Received data: %s", data);
 
-        // Re-send the received data back to the sender
-        uart.send(data, bytesReceived);
+        
     }
     
 }
 
+void uartSendTest_task(){
+    // Get message from the UART queue and send it to the UART interface
+    MessageHandler::Message message;
+    while (true) {
+        if (msgHandler.receive(MessageHandler::QueueType::UART_QUEUE, message)) {
+            uart0.send(message.data(), message.size());
+            ESP_LOGI(pcTaskGetName(nullptr), "Sent data: %s", message.data());
+        } else {
+            ESP_LOGW(pcTaskGetName(nullptr), "Failed to receive message from UART_QUEUE");
+        }
+    }
+}
+
+void dataParserTestTask() {
+    while(true){
+        dataParser.dataParserTask(&msgHandler);
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+
 
 extern "C" void app_main(void) {
+    // Initialize the UART interface
+    uart0.init(msgHandler.getUartEventQueue());
+
     // Create a thread for the UART task
-    esp_pthread_cfg_t cfg = create_config("uartTest_task", 4096, 5);
+    esp_pthread_cfg_t cfg = create_config("uartReceiveTest_task", 4096, 5);
     esp_pthread_set_cfg(&cfg);
-    std::thread testThread(uartTest_task);
+    std::thread testThread(uartReceiveTest_task);
+
+    // Create a thread for the UART send task
+    cfg = create_config("uartSendTest_task", 4096, 5);
+    esp_pthread_set_cfg(&cfg);
+    std::thread sendThread(uartSendTest_task);
+
+    // Create a thread for the DataParser task
+    cfg = create_config("dataParserTestTask", 4096, 5);
+    esp_pthread_set_cfg(&cfg);
+    std::thread parserThread(dataParserTestTask);
 
      // Let the main task do something too
      while (true) {
