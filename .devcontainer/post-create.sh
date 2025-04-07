77d9c8acaa9da71da2180fc778bbe0d2287d47ea
Simon <119622323+ohSpxctre@users.noreply.github.com>
2025-04-07 20:02:30 +0200

    Delete post-create.sh

--- .devcontainer/post-create.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Run doc generation script
-cp /workspaces/BT_UART_Gateway/generate_docs.sh /usr/local/bin/generate_docs.sh
-chmod +x /usr/local/bin/generate_docs.sh
-generate_docs.sh
